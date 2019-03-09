f28e47426e42747f6d49f146cd906223758ea3f1
lingqiz <zlqzcc@gmail.com>
2019-03-08 21:38:05 -0500

    denoise

--- denoiseTest.m
+++ denoiseTest.m
@@ -22,17 +22,29 @@ for idx = 1:6
     testIdx = randi([1, 5000]);
     image = imageTr(testIdx, :);
     noisy = image + normrnd(0, 0.1, size(image));    
-    recon = estimator.estimate(noisy, 'regularization', 0.05);
+    recon = estimator.estimate(noisy, 'regularization', 0.01);
     
     subplot(6, 3, 3 * idx - 2);
-    imshow(reshape(image, imageSize), 'InitialMagnification', 500);
-    
+    imshow(reshape(image, imageSize), 'InitialMagnification', 500);    
     subplot(6, 3, 3 * idx - 1);
-    imshow(reshape(noisy, imageSize), 'InitialMagnification', 500);
-    
+    imshow(reshape(noisy, imageSize), 'InitialMagnification', 500);    
     subplot(6, 3, 3 * idx);
     imshow(reshape(recon, imageSize), 'InitialMagnification', 500);
     
-    snr(1, idx) = imStats(image, noisy);
-    snr(2, idx) = imStats(image, recon);
+    [~, snr(1, idx)] = imStats(image, noisy, false);
+    [~, snr(2, idx)] = imStats(image, recon, false);
 end
+
+%% Evaluation
+regPara = 0 : 0.002 : 0.02;
+nTest = 1e2;
+SNR   = zeros(length(regPara), nTest);
+for idx = 1:nTest
+    image = imageTr(idx, :);
+    noisy = image + normrnd(0, 0.1, size(image));
+    for regIdx = 1:length(regPara)
+        [~, SNR(regIdx, idx)] = imStats(image, estimator.estimate(noisy, 'regularization', regPara(regIdx)), false);
+    end
+end
+
+plot(regPara, mean(SNR, 2)); 
