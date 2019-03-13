b4aa45a82c42ed3f86ee8dce65a4cf1af1ab0f1b
lingqiz <zlqzcc@gmail.com>
2019-03-13 16:40:52 -0400

    add lasso estimator

--- denoiseColor.m
+++ denoiseColor.m
@@ -3,38 +3,36 @@ projectName = 'ColorImagePriors';
 thisImageSet = 'CIFAR_all';
 dataBaseDir = getpref(projectName, 'dataDir');
 
-testData = 'image_cifar_all_gray.mat';
+testData = 'image_cifar_all.mat';
 dataInDir = fullfile(dataBaseDir, thisImageSet, testData);
 load(dataInDir);
 
-imageTr = image_all_gray(1:9.5e4, :);
-imageTe = image_all_gray(9.5e4+1:end, :);
+imageTr = image_all(1:9.5e4, :);
+imageTe = image_all(9.5e4+1:end, :);
 nTestSet = 5e3;
 clear image_all;
 
 %% Gaussian model
 [regBasis, mu] = computeBasisPCA(imageTr, 32);
-estimatorRidge = RidgeGaussianEstimator(eye(1024), regBasis, mu');
+estimatorRidge = RidgeGaussianEstimator(eye(3072), regBasis, mu');
 
-%% LASSO model
-[Z, U, SIG, MU] = whitening(imageTr, 'svd');
-
+%% Sparse (LASSO) model
 basisInDir = fullfile(dataBaseDir, thisImageSet, 'sparse_basis');
-basisName  = 'ica_gray_1600.mat';
+basisName  = 'ica_color_3600_cvg.mat';
 load(fullfile(basisInDir, basisName));
 
-W = Mdl.TransformWeights; nBasis = 1600;
-[~] = visualizeBasis(U * diag(sqrt(SIG)) * W, 32, nBasis, true);
+W = Mdl.TransformWeights;
+[~, U, SIG, MU] = whitening(imageTr, 'svd');
 
 regBasis = U * diag(sqrt(SIG)) * W;
-estimatorLasso = LassoGaussianEstimator(eye(1024), regBasis, MU');
+estimatorLasso = LassoGaussianEstimator(eye(3072), regBasis, MU');
 
-%% Baseline model (lowpass filter)
-lo_filt = binomialFilter(3) * binomialFilter(3)';
-filterRecon = @(image) rconv2(image,lo_filt);
+%% Visualization
+[~] = visualizeBasis(estimatorRidge.Basis, 32, size(estimatorRidge.Basis, 2), false);
+[~] = visualizeBasis(estimatorLasso.Basis, 32, size(estimatorLasso.Basis, 2), false);
 
 %% Denoising test
-imageSize = [32, 32];
+imageSize = [32, 32, 3];
 figure();
 for idx = 1:6
     testIdx = randi([1, nTestSet]);
@@ -43,30 +41,25 @@ for idx = 1:6
     
     reconGauss = estimatorRidge.estimate(noisy, 'regularization', 0.01);
     reconLasso = estimatorLasso.estimate(noisy, 'regularization', 0.01);
-    reconFilt  = filterRecon(reshape(image, imageSize));
     
-    subplot(6, 5, 5 * idx - 4);
+    subplot(6, 4, 4 * idx - 3);
     imshow(reshape(image, imageSize), 'InitialMagnification', 500);    
-    subplot(6, 5, 5 * idx - 3);
+    subplot(6, 4, 4 * idx - 2);
     imshow(reshape(noisy, imageSize), 'InitialMagnification', 500);    
-    subplot(6, 5, 5 * idx - 2);
-    imshow(reconFilt, 'InitialMagnification', 500);
-    subplot(6, 5, 5 * idx - 1);
+    subplot(6, 4, 4 * idx - 1);
     imshow(reshape(reconGauss, imageSize), 'InitialMagnification', 500);
-    subplot(6, 5, 5 * idx);
+    subplot(6, 4, 4 * idx);
     imshow(reshape(reconLasso, imageSize), 'InitialMagnification', 500);
     
-    imStats(reshape(image, imageSize), reconFilt, true);
     imStats(image, reconGauss, true);
     imStats(image, reconLasso, true);
 end
 
 %% Evaluation
-regPara_ridge = 0 : 0.001 : 0.01;
-regPara_lasso = 0 : 0.001 : 0.01;
+regPara_ridge = 0 : 0.001 : 0.02;
+regPara_lasso = 0 : 0.001 : 0.02;
 
-nTest = 5e1; 
-nReg  = length(regPara_ridge);
+nTest = 1e1; nReg = length(regPara_ridge);
 
 SNR_ridge = zeros(nReg, nTest);
 SNR_lasso = zeros(nReg, nTest);
@@ -75,10 +68,11 @@ rmse_ridge = zeros(nReg, nTest);
 rmse_lasso = zeros(nReg, nTest);
 
 for idx = 1:nTest
+    fprintf('Cross Validation Iteration %d / %d \n', idx, nTest);
     image = imageTr(randi([1, nTestSet]), :);
     noisy = image + normrnd(0, 0.1, size(image));
     
-    parfor regIdx = 1:nReg
+    for regIdx = 1:nReg
         [rmse_ridge(regIdx, idx), SNR_ridge(regIdx, idx)] = ...
             imStats(image, estimatorRidge.estimate(noisy, 'regularization', regPara_ridge(regIdx)), false);
         
@@ -87,6 +81,7 @@ for idx = 1:nTest
     end
 end
 
+%% Plot SNR
 figure();
 p1 = subplot(1, 2, 1);
 plot(regPara_ridge, mean(SNR_ridge, 2), '-o', 'LineWidth', 2);
@@ -103,6 +98,9 @@ ylm2 = ylim;
 ylim(p1, [min([ylm1, ylm2]), max([ylm1, ylm2])]);
 ylim(p2, [min([ylm1, ylm2]), max([ylm1, ylm2])]);
 
+suptitle('Color Denoising, CIFAR, Noise Var: 0.1');
+
+%% Plot RMSE
 figure;
 p1 = subplot(1, 2, 1);
 plot(regPara_ridge, mean(rmse_ridge, 2), '-o', 'LineWidth', 2);
@@ -118,4 +116,3 @@ ylm2 = ylim;
 
 ylim(p1, [min([ylm1, ylm2]), max([ylm1, ylm2])]);
 ylim(p2, [min([ylm1, ylm2]), max([ylm1, ylm2])]);
-
